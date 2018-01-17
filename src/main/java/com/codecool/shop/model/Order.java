6796d58cbeac1092872f867ec54c791aae40e78c
Nora Mecs-Berta <ms.noraberta@gmail.com>
2018-01-17 23:42:11 +0100

    models documentation

--- src/main/java/com/codecool/shop/model/Order.java
+++ src/main/java/com/codecool/shop/model/Order.java
@@ -4,6 +4,14 @@ import com.codecool.shop.dao.implementation.OrderDaoMem;
 
 import java.util.HashMap;
 
+/**
+ * This is the Order class based on BaseModel.
+ * <p>The purpose of this class is to be able to work with the orders (as objects)</p>
+ * <p>Here you can access the order's products, you can add / delete / find products,
+ * get the total price of the order, all products and the user id.</p>
+ * <p>To be more practical: here you can get the features of the webshop's orders and also you can modifiy the
+ * products of the order</p>
+ */
 public class Order extends BaseModel {
 
     private HashMap<Product, Integer> products = new HashMap<>();
@@ -12,25 +20,41 @@ public class Order extends BaseModel {
     private Float totalPrice = 0f;
 
 
+    /**
+     * Constructor, set the user id
+     * @param userId belongs to the user, whose order it is
+     */
     public Order(Integer userId) {
         super("order");
         this.userId = userId;
         OrderDaoMem.getInstance().add(this);
     }
 
-
+    /**
+     * You can add one product to the order
+     * @param product the item what you would like to add
+     */
     public void add(Product product) {
         products.merge(product, 1, (a, b) -> a + b);
         numberOfProducts++;
         totalPrice += product.getDefaultPrice();
     }
 
+    /**
+     * You can remove product from the order
+     * @param id the id of the product what should be deleted
+     */
     public void remove(int id) {
         Product productToRemove = find(id);
         products.remove(productToRemove);
         totalPrice -= productToRemove.getDefaultPrice();
     }
 
+    /**
+     * You can find a product in the order
+     * @param id the product's id what should be found
+     * @return the product if exist, otherwise null
+     */
     public Product find(int id)  {
         for (Product product : products.keySet()) {
             if (product.id == id) {
@@ -40,12 +64,24 @@ public class Order extends BaseModel {
         return null;
     }
 
+    /**
+     * @return all of the products in the order
+     */
     public HashMap<Product, Integer> getAll() { return products; }
 
+    /**
+     * @return the user id of the user whose order it is
+     */
     public Integer getUserId() { return userId; }
 
+    /**
+     * @return an integer, the number of products in the order
+     */
     public Integer getNumberOfProducts() { return numberOfProducts; }
 
+    /**
+     * @return the total price of the order
+     */
     public Float getTotalPrice() { return totalPrice; }
 
 }
--- src/main/java/com/codecool/shop/model/Product.java
+++ src/main/java/com/codecool/shop/model/Product.java
@@ -2,13 +2,29 @@ package com.codecool.shop.model;
 
 import java.util.Currency;
 
+/**
+ * This is the Product class based on BaseModel.
+ * <p>The purpose of this class is to be able to work with the products (as objects)</p>
+ * <p>Here you can modify and access the default price, default currency, the product category and the supplier of the products.</p>
+ * <p>To be more practical: here you can get the features of webshop products and also you can modifiy them</p>
+ */
 public class Product extends BaseModel {
+
 
     private float defaultPrice;
     private Currency defaultCurrency;
     private ProductCategory productCategory;
     private Supplier supplier;
 
+    /**
+     * Constructor, set the name, defaultPrice, currency, description, productCategory and supplier of the Product
+     * @param name name of the actual product
+     * @param defaultPrice the selling price
+     * @param currencyString the currency of the product
+     * @param description description about the product
+     * @param productCategory products grouped by some properties. One product category covers more products
+     * @param supplier the supplier of the actual product
+     */
     public Product(String name, float defaultPrice, String currencyString, String description, ProductCategory productCategory, Supplier supplier) {
         super(name, description);
         this.setPrice(defaultPrice, currencyString);
@@ -16,12 +32,18 @@ public class Product extends BaseModel {
         this.setProductCategory(productCategory);
     }
 
+    /**
+     * @return defaultPrice of the actual product
+     */
     public float getDefaultPrice() { return defaultPrice; }
 
     public void setDefaultPrice(float defaultPrice) {
         this.defaultPrice = defaultPrice;
     }
 
+    /**
+     * @return the defaultCurrency of the actual product
+     */
     public Currency getDefaultCurrency() {
         return defaultCurrency;
     }
@@ -34,27 +56,50 @@ public class Product extends BaseModel {
         return String.valueOf(this.defaultPrice) + " " + this.defaultCurrency.toString();
     }
 
+    /**
+     * Set the price and the related currency of the actual product
+     * @param price
+     * @param currency
+     */
     public void setPrice(float price, String currency) {
         this.defaultPrice = price;
         this.defaultCurrency = Currency.getInstance(currency);
     }
 
+    /**
+     * @return the productCategory of the actual product
+     */
     public ProductCategory getProductCategory() {
         return productCategory;
     }
 
+    /**
+     * Set the productCategory of the actual product.
+     * @param productCategory
+     */
     public void setProductCategory(ProductCategory productCategory) {
         this.productCategory = productCategory;
     }
 
+    /**
+     * @return the supplier of the actual product
+     */
     public Supplier getSupplier() {
         return supplier;
     }
 
+    /**
+     * Set the supplier of the actual product.
+     * @param supplier
+     */
     public void setSupplier(Supplier supplier) {
         this.supplier = supplier;
     }
 
+    /**
+     * @return the product's name, defaultPrice, defaultCurrency, productCategory and supplier
+     * in printable format
+     */
     @Override
     public String toString() {
         return String.format("id: %1$d, " +
