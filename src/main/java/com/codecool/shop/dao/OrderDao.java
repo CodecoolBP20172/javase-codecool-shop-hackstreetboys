fd47ba52987377768b134809a4d17ed43c409b4b
Nora Mecs-Berta <ms.noraberta@gmail.com>
2018-01-18 11:00:28 +0100

    dao interfaces and ProductDaoMem implemented

--- src/main/java/com/codecool/shop/dao/OrderDao.java
+++ src/main/java/com/codecool/shop/dao/OrderDao.java
@@ -5,14 +5,35 @@ import com.codecool.shop.model.Order;
 
 import java.util.List;
 
+/**
+ * This interface is part of the dao, furthermore it is responsible for the webshop's orders.
+ * <p>It provides access to the database or the other persistence storage (memory).</p>
+ * (It depends on the implementing class.)
+ * <p>Here you can add / find / remove orders and also you can access all of them.</p>
+ */
 public interface OrderDao {
-
+    /**
+     * Method signature of adding orders
+     * @param order collection of products what the user would like to buy
+     */
     void add(Order order);
+
+    /**
+     * Method signature of finding orders
+     * @param id unique identifier what belongs to one specific order what you would like to find
+     * @return the order which belongs to that unique id
+     */
     Order find(int id);
+
+    /**
+     * Method signature of removing orders
+     * @param id unique identifier what belongs to one specific order what you would like to remove
+     */
     void remove(int id);
 
-    //HashMap<Product, Integer> getAll();
+    /** Method signature of accessing all orders.
+     * @return all of the orders
+     */
     List<Order> getAll();
-    /*List<Product> getBy(Supplier supplier);
-    List<Product> getBy(ProductCategory productCategory);*/
+
 }
--- src/main/java/com/codecool/shop/dao/ProductDao.java
+++ src/main/java/com/codecool/shop/dao/ProductDao.java
@@ -6,14 +6,50 @@ import com.codecool.shop.model.ProductCategory;
 
 import java.util.List;
 
+/**
+ * This interface is part of the dao, furthermore it is responsible for the webshop's products.
+ * <p>It provides access to the database or the other persistence storage (memory).</p>
+ * (It depends on the implementing class.)
+ * <p>Here you can add / find / remove products and also you can access all of them
+ * or the ones belongs to a specific supplier or product category.</p>
+ */
 public interface ProductDao {
-
+    /**
+     * Method signature of adding products
+     * @param product this is the product you would like to add
+     */
     void add(Product product);
+
+    /**
+     * Method signature of finding products
+     * @param id unique identifier that belongs to one specific product what you would like to find
+     * @return the product which belongs to that unique id
+     */
     Product find(int id);
+
+    /**
+     * Method signature of removing products
+     * @param id unique identifier that belongs to one specific product what you would like to remove
+     */
     void remove(int id);
 
+    /**
+     * Method signature of accessing all products
+     * @return all products
+     */
     List<Product> getAll();
-    List<Product> getBy(Supplier supplier);
-    List<Product> getBy(ProductCategory productCategory);
 
+    /**
+     * Method signature of accessing those products which have the same supplier
+     * @param supplier whose products you would like to access
+     * @return the products of the specific supplier
+     */
+    List<Product> getBy(Supplier supplier);
+
+    /**
+     * Method signature of accessing those products which have the same product category
+     * @param productCategory the products you would like to access belong to this product category
+     * @return the products belongs to that specific product category
+     */
+    List<Product> getBy(ProductCategory productCategory);
 }
